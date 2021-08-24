22be0930365c327448d5c74f987f2cfdc37b4275
Gaurav Koley <arkokoley@live.in>
2021-08-24 17:49:57 +0530

    Update engine.rb

--- lib/rails_admin_tag_list/engine.rb
+++ lib/rails_admin_tag_list/engine.rb
@@ -2,7 +2,7 @@ module RailsAdminTagList
   class Engine < ::Rails::Engine
     initializer 'rails_admin_rag_list.action_controller' do |app|
       ActiveSupport.on_load :action_controller do
-        helper RailsAdminTagList::SuggestionsHelper
+        ::ActionController::Base.send(:include, RailsAdminTagList::SuggestionsHelper)
       end
     end
   end
